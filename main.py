8e78493a764f9ea5e1b7e992eacc4c04eedf9fd8
Michał <durajczykmichal@gmail.com>
2022-12-30 17:39:48 +0100

    Initial commit

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ from flet import *
 import requests
 import datetime
 
+api_key = '8d3b0a2d8e81b13ba60923d557f702b2'
+
+response = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={api_key}')
+
+print(response)
